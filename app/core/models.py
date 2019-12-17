05e581851339aca0323fc653fc493ff77363fae4
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-12-17 14:09:16 -0800

    Added validation.

--- app/core/models.py
+++ app/core/models.py
@@ -10,8 +10,9 @@ class UserManager(BaseUserManager):
 
     # Create user
     def create_user(self, email, password=None, **extra_fields):
-
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("Users must have an email address!")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,3 +16,18 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+
+        """Test the email for a new user is normalized"""
+        email = "test@GMAIL.com"
+        user = get_user_model().objects.create_user(email, "blahblah")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+
+        """Test that creating user w/o email raises error"""
+        with self.assertRaises(ValueError):
+
+            get_user_model().objects.create_user(None, "testing123")
